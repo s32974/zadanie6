7aed76a962cc803d43e08ebbbc0419219d218369
s32974 <s32974@pjwstk.edu.pl>
2025-04-17 14:40:24 +0200

    początek

---
+++ src/Vehicle.java
@@ -0,0 +1,14 @@
+public class Vehicle {
+
+
+    private void start() {
+        System.out.println("Vehicle started");
+    }
+
+
+    private void stop() {
+        System.out.println("Vehicle stopped");
+    }
+
+
+}
